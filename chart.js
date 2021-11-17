1e3c0e4e665c12b316a5de103fd015ecb5156246
Stein Bergervoet <stein.bergervoet@gmail.com>
2021-11-17 17:38:00 +0100

    Add transitions to chart

--- chart.js
+++ chart.js
@@ -6,6 +6,7 @@ overweightChart.attr("width", chartWidth).attr("height", chartHeight);
 let overweightChartGroup = overweightChart.append("g");
 
 let x;
+let y;
 
 // Main function.
 function main() {
@@ -14,20 +15,17 @@ function main() {
 
 // Function to draw the initial chart.
 function drawChart(data) {
-  const padding = 32;
+  const padding = 48;
 
-  x = d3
+  x = d3 // Scale band for the x axis.
     .scaleBand()
     .domain(d3.range(data.length))
     .range([0, chartWidth - padding * 2]);
 
-  overweightChartGroup
+  overweightChartGroup // Add the x axis to the svg element.
     .append("g")
     .attr("id", "xAxis")
-    .attr(
-      "transform",
-      `translate(${padding * 1.5}, ${chartHeight - padding + 8})`
-    )
+    .attr("transform", `translate(${padding}, ${chartHeight - padding})`)
     .call(
       d3
         .axisBottom(x)
@@ -35,15 +33,15 @@ function drawChart(data) {
         .tickFormat(i => `${data[i].year}`)
     );
 
-  const y = d3
+  y = d3 // Scale for the y axis.
     .scaleLinear()
     .domain([100, 0])
-    .range([0, chartHeight - padding]);
+    .range([0, chartHeight - padding * 2]);
 
-  overweightChartGroup
+  overweightChartGroup // Add the y axis to the svg element.
     .append("g")
     .attr("id", "yAxis")
-    .attr("transform", `translate(${padding * 1.5}, 8)`)
+    .attr("transform", `translate(${padding}, ${padding})`)
     .call(
       d3
         .axisLeft(y)
@@ -56,7 +54,52 @@ function drawChart(data) {
 }
 
 // Function to update the chart on hover.
-function updateChart(data) {}
+function updateChart(data) {
+  const padding = 48;
+  const width = (chartWidth - 2 * padding) / data.length - 4; // Width of the bars.
+
+  overweightChartGroup
+    .selectAll("rect")
+    .data(data)
+    .join(
+      enter => {
+        return enter
+          .append("rect")
+          .transition()
+          .duration(400)
+          .attr("fill", d => color(d.overweight))
+          .attr("width", width)
+          .attr(
+            "height",
+            d => (d.overweight / 100) * (chartHeight - 2 * padding)
+          )
+          .attr(
+            "transform",
+            (d, i) =>
+              `translate(${x(i) + padding + 3}, ${y(d.overweight) + padding})`
+          );
+      },
+      update => {
+        return update
+          .transition()
+          .duration(400)
+          .attr("fill", d => color(d.overweight)) // Update the color
+          .attr("width", (chartWidth - 2 * padding) / data.length - 8) // Update the width of the rectangle.
+          .attr(
+            "height",
+            d => (d.overweight / 100) * (chartHeight - 2 * padding) // Update the height of the rectangles.
+          )
+          .attr(
+            "transform",
+            (d, i) =>
+              `translate(${x(i) + padding + 5}, ${y(d.overweight) + padding})` // Update the transform of the rectangles.
+          );
+      },
+      exit => {
+        return exit.transition().duration(400).style("opacity", 0).remove(); // Delete left over rectangles.
+      }
+    );
+}
 
 // Function to rescale the x axis of the chart.
 function rescale(data) {
